b337125a2ea0aae5a515b22d5ba83233e34a5983
ThienlocTran <tranthienloc21102005@gmail.com>
2025-07-18 10:25:57 +0700

    commit 3: write method EmployeeController and extends CrudController witsh <UserAccount>

--- src/main/java/com/team4/quanliquanmicay/Controller/EmployeeController.java
+++ src/main/java/com/team4/quanliquanmicay/Controller/EmployeeController.java
@@ -1,5 +1,12 @@
 package com.team4.quanliquanmicay.Controller;
 
-public class EmployeeController {
-    
+import com.team4.quanliquanmicay.Entity.UserAccount;
+
+/**
+ *
+ * @author Team4
+ */
+public interface EmployeeController extends CrudController<UserAccount> {
+    void loadRoles();           // Tải danh sách vai trò vào ComboBox
+    void validateEmployee();    // Kiểm tra hợp lệ thông tin nhân viên
 }
